835e7a46fc416c26ba56726cbb4fbee3e0d19b81
Jairo Cugliari <jairocugliari@gmail.com>
2023-07-16 18:41:35 +0200

    new R config

---
+++ .binder/install.R
@@ -0,0 +1,9 @@
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+#install.packages("shinydashboard")
+install.packages('leaflet')
+install.packages('fda')
+install.packages('fts')
+install.packages('rainbow')
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # test-binder
+
+<!-- badges: start -->
+[![Launch binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/cugliari/test-binder/main)
+<!-- badges: end -->
+
